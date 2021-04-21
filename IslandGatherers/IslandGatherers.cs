95220bb95b6e0d6f9805696ddeab134acc8b9eaa
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-04-20 19:27:30 -0500

    Start of project

---
+++ IslandGatherers/IslandGatherers.cs
@@ -0,0 +1,22 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IslandGatherers
+{
+    public class IslandGatherers : Mod
+    {
+        internal static IMonitor monitor;
+        internal static IModHelper modHelper;
+
+        public override void Entry(IModHelper helper)
+        {
+            // Set up the monitor and helper
+            monitor = Monitor;
+            modHelper = helper;
+        }
+    }
+}
